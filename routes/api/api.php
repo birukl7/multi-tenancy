39d93cd0f77fc948bce7075db5bf0671cf4ebaaa
Biruk Lemma <biruklemmadebela@gmail.com>
2024-10-18 12:16:38 +0300

    Multi-tenancy

---
+++ routes/api/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        // your actual routes
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->middleware('auth:sanctum');
+    });
+}
+
+
